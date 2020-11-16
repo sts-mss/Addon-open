60c695ccc0891e4b24ccde53f9555c2216f2ca1a
darkz3ro <darkz3ro@localhost.localdomain>
2020-11-16 13:24:09 +0200

    added new deployment addons

---
+++ deployment-apps/TA-microsoft_ad_inputs/bin/runpowershell-nt6-repl-stat.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+
+:: ######################################################
+:: #
+:: # Splunk for Microsoft Active Directory
+:: # 
+:: # Copyright (C) 2016 Splunk, Inc.
+:: # All Rights Reserved
+:: #
+:: ######################################################
+
+set SplunkApp=Splunk_TA_microsoft_ad
+
+powershell.exe  -command "C:\Program*\SplunkUniversalForwarder\etc\apps\TA-microsoft_ad_inputs\bin\powershell\nt6-repl-stat.ps1"
